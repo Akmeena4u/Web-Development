7fc80e34c06e1020e3be2f7c9a1aaed6f7b6f240
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-29 17:50:09 +0530

    Create variables.php

---
+++ PHP/variables.php
@@ -0,0 +1,13 @@
+#Think of variables as containers for storing data.
+#In PHP, a variable starts with the $ sign, followed by the name of the variable . PHP is a Loosely Typed Language, here no need to declare variables:
+#The PHP echo statement is often used to output data to the screen.
+
+
+
+<?php
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+
+echo  $txt ;
+?>
