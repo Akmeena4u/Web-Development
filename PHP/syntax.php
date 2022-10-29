236f1c116d8353bc6dea0e7f61355354a9c9c863
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-29 17:40:08 +0530

    Create syntax.php

---
+++ PHP/syntax.php
@@ -0,0 +1,35 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?>
+
+</body>
+</html>
+
+
+
+/*
+
+A PHP script is executed on the server, and the plain HTML result is sent back to the browser.
+
+A PHP script can be placed anywhere in the document.
+
+A PHP script starts with <?php and ends with ?>:
+
+PHP script that uses a built-in PHP function "echo" to output the text "Hello World!" on a web page:
+
+Note: PHP statements end with a semicolon (;).
+
+in php everything is not case sensitive except variable name
+
+
+
+*/
