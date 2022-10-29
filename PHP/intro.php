25945e55299241b01030f02858069045f6f41d52
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-29 17:31:18 +0530

    Create intro.php

---
+++ PHP/intro.php
@@ -0,0 +1,32 @@
+
+
+<?php  //-   Opening tag of a block
+
+echo "hello" // echo is a command to print to the screen
+
+;   //-  terminates the command
+
+?> //closing tag of a block
+
+/*
+
+PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.
+PHP is an acronym for "PHP: Hypertext Preprocessor"
+
+What is a PHP File?
+PHP files can contain text, HTML, CSS, JavaScript, and PHP code
+PHP code is executed on the server, and the result is returned to the browser as plain HTML
+PHP files have extension ".php"
+
+What Can PHP Do?
+PHP can generate dynamic page content
+PHP can create, open, read, write, delete, and close files on the server
+PHP can collect form data
+PHP can send and receive cookies
+PHP can add, delete, modify data in your database
+PHP can be used to control user-access
+PHP can encrypt data
+
+PHP 7 supports new operators (like the spaceship operator: <=>)
+
+*/
