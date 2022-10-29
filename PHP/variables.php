c114b6e02fb577b069d73a57227fc07ee60a62df
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-29 18:09:27 +0530

    Update variables.php

--- PHP/variables.php
+++ PHP/variables.php
@@ -4,10 +4,20 @@
 
 
 
-<?php
-$txt = "Hello world!";
-$x = 5;
-$y = 10.5;
 
-echo  $txt ;
+<?php
+// Variables are containers for storing information
+// Starts with a $
+$name = "Shubham";
+// Variables are case sensitive
+$namE = "Capital wala Shubham";
+$income = 200.8;
+
+echo "This guy is $name and his income is Rs. $income<br>";
+
+echo "$namE is a good boy<br>";
+echo "$name is the real gangsta<br>";
+
+
 ?>
+
