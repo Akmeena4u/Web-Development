2f85f83209bdaffcdd6c121c2459d859333d279c
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-11-01 19:43:07 +0530

    Create functions.php

---
+++ PHP/functions.php
@@ -0,0 +1,37 @@
+#A function is a block of statements that can be used repeatedly in a program.
+//A function will not execute automatically when a page loads.
+//A function will be executed by a call to the function.
+
+
+<?php
+echo "This is php tutorial on functions <br>";
+
+function processMarks($marksArr){
+    $sum = 0;
+    foreach ($marksArr as $value) {
+        $sum += $value;
+    }
+    return $sum;
+}
+
+function avgMarks($marksArr){
+    $sum = 0;
+    $i = 1;
+    foreach ($marksArr as $value) {
+        $sum += $value;
+        $i++;
+    }
+    return $sum/$i;
+}
+
+$rohanDas = [34, 98, 45, 12, 98, 93];
+// $sumMarks = processMarks($rohanDas);
+$sumMarks = avgMarks($rohanDas);
+
+$harry = [99, 98, 93, 94, 17, 100];
+// $sumMarksHarry = processMarks($harry);
+$sumMarksHarry = avgMarks($harry);
+echo "Total marks scored by Rohan out of 600 is $sumMarks <br>";
+echo "Total marks scored by Harry out of 600 is $sumMarksHarry";
+
+?>
